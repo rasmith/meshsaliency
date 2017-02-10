78b467c07ac785e75bf51987681b6bba941c28ac
Randall Smith <rsmith0x0@gmail.com>
2017-02-09 22:02:46 -0600

    batch rendering work

--- main.cc
+++ main.cc
@@ -11,12 +11,19 @@ Eigen::MatrixXi F;
 using std::placeholders::_1;
 
 struct RenderRequest {
-  unsigned int count;
-  unsigned int limit;
 };
 
-bool render_to_png(igl::viewer::Viewer& viewer, RenderRequest* request) {
-  if (request->count < request->limit) {
+struct RenderRequests {
+  std::vector<RenderRequest> request_list;
+  int current_request;
+};
+
+void generate_render_requests(RenderRequests* requests) {
+  requests->current_request = 0;
+}
+
+bool render_to_png(igl::viewer::Viewer& viewer, RenderRequests* requests) {
+  if (requests->current_request < requests->request_list.size()) {
     // Allocate temporary buffers
     Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> R(1280, 800);
     Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> G(1280, 800);
@@ -28,8 +35,8 @@ bool render_to_png(igl::viewer::Viewer& viewer, RenderRequest* request) {
 
     // Save it to a PNG
     igl::png::writePNG(R, G, B, A,
-		       "out" + std::to_string(request->count) + ".png");
-    ++request->count;
+		       "out" + std::to_string(requests->current_request) + ".png");
+    ++requests->current_request;
   }
   return true;
 }
@@ -40,12 +47,13 @@ int main(int argc, char* argv[]) {
   // Load a mesh in OFF format.
   igl::readOFF(model_path, V, F);
 
-  // Setup a render request.
-  RenderRequest render_request = {0, 3};
+  // Setup a render requests.
+  RenderRequests render_requests;
+  generate_render_requests(&render_requests);
 
   // Plot the mesh.
   igl::viewer::Viewer viewer;
-  viewer.callback_post_draw = std::bind(render_to_png, _1, &render_request);
+  viewer.callback_post_draw = std::bind(render_to_png, _1, &render_requests);
   viewer.data.set_mesh(V, F);
   viewer.launch();
 }
