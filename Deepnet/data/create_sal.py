176610bf47ce5bb424918bd55d81c00ab0ad8ecb
SoumyajitG <smjtgupta@gmail.com>
2017-05-07 13:24:11 -0500

    Initial commit for deep arch

---
+++ Deepnet/data/create_sal.py
@@ -0,0 +1,29 @@
+# import libraries
+
+from __future__ import absolute_import
+
+import numpy as np
+import scipy.io as sio
+import h5py
+import os
+import sys
+import matplotlib.pyplot as plt
+import glob
+import cv2
+
+# load data
+
+path = 'off/'
+in_files = os.listdir(path)
+dir_len = len(in_files)
+
+sal_im = np.zeros((dir_len*11,256,256),dtype=np.float)
+
+for i in range(dir_len):
+	for j in range(11):
+		img = cv2.imread(path + in_files[i] + '/sal/out'+str(j)+'.png',cv2.IMREAD_GRAYSCALE)
+		img = np.expand_dims(img,0)
+		img = np.array([img])
+		sal_im[i*11+j,:,:] = img
+
+np.save('sal_set.npy',sal_im)
